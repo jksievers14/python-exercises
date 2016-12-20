732e6bf3c7578dd2aa3b72f21f55d4cdf2e9bb04
jksievers14 <jksievers14@gmail.com>
2016-12-20 14:43:31 -0500

    Add files via upload

---
+++ GithubPractice12:20:16.py
@@ -0,0 +1,36 @@
+'''
+exercise 1
+'''
+start = 100
+retrun=1.1
+print(start * retrun**7)
+print(start * retrun**7)
+'''
+excersise 2
+'''
+savings = 100
+factor = 1.10
+
+print(savings*factor)
+'''
+exersise 3
+'''
+desc = "compund interest"
+profitable=True
+'''
+exercise 4
+'''
+print(type("compund interest"))
+print(type(profitable))
+print(type(savings))
+print(type(factor))
+
+print('I start with $100 and now I have 194.87. Great!')
+      
+
+
+
+
+
+      
+
